aec0471c8da501e71c22a98cb9e0e5a5657cf0eb
cybertronayush <44187942+cybertronayush@users.noreply.github.com>
2020-10-28 23:47:03 +0530

    Add files via upload

---
+++ 10DaysOfcode/TrailingZero.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int findZeros(int n){
+	int ans=0;
+	for(int d=5;n/d>=1;d*5){
+		ans+=n/d;
+	}
+	return ans;
+}
+int main(){
+ +
+	long long int n;
+	cin>>n;
+	cout<<findZeros(n)<<endl;
+	return 0;
+
+
+}
